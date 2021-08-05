08dcf8085ba44ef84551e94bb9a8f110ec4652e3
namita514 <67231912+namita514@users.noreply.github.com>
2021-08-05 12:43:32 +0530

    implemented shared pref.

--- Task1/CounterApp2/app/src/main/java/com/example/counterapp/MainActivity.java
+++ Task1/CounterApp2/app/src/main/java/com/example/counterapp/MainActivity.java
@@ -10,19 +10,48 @@ import com.example.counterapp.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private int qty=0;
     private ActivityMainBinding b;
+    private SharedPreferences mPreferences;
+    private String sharedPrefFile =
+            "com.example.android.counterapp";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        b=ActivityMainBinding.inflate(getLayoutInflater());
+        b = ActivityMainBinding.inflate(getLayoutInflater());
 
         setContentView(b.getRoot());
+
+        // initialising share preferences
+        mPreferences = getSharedPreferences(
+                sharedPrefFile, MODE_PRIVATE);
+
+            qty=mPreferences.getInt("Quantity",0);
+            b.iniText.setText(String.format("%s", qty));
+
+        }
+    //implementing onPause() lifecycle method.
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        // get an editor for shared preferences
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putInt("Quantity",qty);
+        preferencesEditor.apply();
     }
     //decrementing quantity
     public void decQty(View view) {
-        b.iniText.setText(""+ qty--);
+        qty++;
+        b.iniText.setText(String.format("%s", qty));
     }
     //incrementing quantity
     public void incQty(View view) {
-        b.iniText.setText(""+ qty++);
+        qty--;
+        b.iniText.setText(String.format("%s", qty));
     }
+
+
+
+
+
+
 }
